88d8cb74a50c58e0c5c0c9a3e7b93fe301a51339
Alexey Kachkaev <akachkaev@gmail.com>
2022-05-01 07:35:19 +0500

    First try 1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,8 +12,11 @@ router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
 router.register(r'follow', FollowViewSet)
 
 urlpatterns = [
-    path('v1/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('v1/jwt/create/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('v1/jwt/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('v1/jwt/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
     path('v1/', include(router.urls)),
 ]
